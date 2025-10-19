882af2f626df57f6b015d82077aadaabb2a43608
Adrian <adi.precub@gmail.com>
2025-10-19 15:49:37 +0200

    add carbon aware and other jobs

--- jobrunr/src/main/java/com/jobrunr/Main.java
+++ jobrunr/src/main/java/com/jobrunr/Main.java
@@ -2,9 +2,13 @@ package com.jobrunr;
 
 import org.jobrunr.configuration.JobRunr;
 import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.scheduling.carbonaware.CarbonAware;
+import org.jobrunr.scheduling.cron.Cron;
 import org.jobrunr.storage.InMemoryStorageProvider;
 
 import java.time.LocalDateTime;
+
+import static java.time.temporal.ChronoUnit.HOURS;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,5 +31,16 @@ public class Main {
 
         //recurring job
         BackgroundJob.scheduleRecurrently("my-recurring-job", "*/1 * * * *", () -> myService.recurringJob());
+
+        //every 5 minutes job
+        BackgroundJob.scheduleRecurrently(Cron.every5minutes(), () -> myService.every5MinJob());
+
+        //last day of the month job
+        BackgroundJob.scheduleRecurrently(Cron.lastDayOfTheMonth(23), () -> myService.lastDayOfMonthJob());
+
+        //carbon aware job
+        LocalDateTime now = LocalDateTime.now();
+        BackgroundJob.schedule(CarbonAware.between(now, now.plus(5, HOURS)), () -> myService.carbonAwareJob());
     }
+
 }
--- jobrunr/src/main/java/com/jobrunr/MyService.java
+++ jobrunr/src/main/java/com/jobrunr/MyService.java
@@ -10,7 +10,19 @@ public class MyService {
         System.out.println("Scheduled Job executed!");
     }
 
+    public void every5MinJob() {
+        System.out.println("Every 5 Minutes Job executed!");
+    }
+
     public void recurringJob() {
         System.out.println("Recurring Job executed!");
+    }
+
+    public void lastDayOfMonthJob() {
+        System.out.println("Last Day of the Month Job executed!");
+    }
+
+    public void carbonAwareJob() {
+        System.out.println("This job runs when the carbon intensity is low");
     }
 }
